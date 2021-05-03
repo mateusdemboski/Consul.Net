898e1981b1869f3d58ddbce60717a25979889eda
Mateus Demboski <mateusdemboski@gmail.com>
2021-05-03 16:00:35 -0300

    feat(agentService): add deregister agent service resource

--- src/Consul.Net/IAgentServiceResources.cs
+++ src/Consul.Net/IAgentServiceResources.cs
@@ -12,6 +12,16 @@ namespace Consul.Net
     public interface IAgentServiceResources
     {
         /// <summary>
+        /// Deregister a service on the Consul agent.
+        /// </summary>
+        /// <remarks>The agent will take care of deregistering the service with the catalog. If there is an associated check, that is also deregistered.</remarks>
+        /// <param name="serviceId">The service ID to be deregistered.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/>.</param>
+        /// <returns>A completed Task if the service was successful deregistered. Otherwise, throws an exception.</returns>
+        /// <exception cref="HttpRequestException">Throwed when an unsuccessful status code was returnd by the service.</exception>
+        Task DeregisterAsync(string serviceId, CancellationToken cancellationToken = default);
+
+        /// <summary>
         /// Adds a new service, with optional health checks, to the Consul agent.
         /// </summary>
         /// <param name="agentServiceRegister">The <see cref="AgentServiceRegister"/> structure that will be registerd on Consul.</param>
--- test/Consul.Net.UnitTests/AgentServiceResourcesTests.cs
+++ test/Consul.Net.UnitTests/AgentServiceResourcesTests.cs
@@ -153,6 +153,110 @@ namespace Consul.Net
                     .Which
                         .StatusCode.Should().Be(expectedErrorStatusCode);
         }
+        /////////////////////////////////////////
+
+        [TestMethod]
+        public void DeregisteringWithoutServiceIdShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var resource = CreateResource();
+
+            // Act
+            Func<Task> registeringWithoutAgentServiceRegister =
+                async () => await resource.DeregisterAsync(default!).ConfigureAwait(false);
+
+            // Assert
+            _ = registeringWithoutAgentServiceRegister.Should().ThrowExactly<ArgumentNullException>()
+                .Which.ParamName.Should().Be("serviceId");
+        }
+
+        [TestMethod]
+        public async Task DeregisteringShouldPutRequestOnConsulAsync()
+        {
+            // Arrange
+            var agentServiceRegister = new AgentServiceRegister();
+
+            using var emptyContent = new StringContent(string.Empty);
+
+            using var messageHandler = Substitute.For<HttpMessageHandler>();
+            using var okMessage = new HttpResponseMessage
+            {
+                Content = emptyContent,
+                StatusCode = OK,
+            };
+
+#pragma warning disable NS1000 // Non-virtual setup specification.
+#pragma warning disable NS1004 // Argument matcher used with a non-virtual member of a class.
+            _ = messageHandler
+                .Protected(
+                    "SendAsync",
+                    Arg.Is<HttpRequestMessage>(
+                        request => request.Method == Put
+                        && request.RequestUri == new Uri("http://test/v1/api/agent/service/deregister/service-id")
+                        && request.Content == emptyContent),
+                    Arg.Any<CancellationToken>())
+#pragma warning restore NS1004 // Argument matcher used with a non-virtual member of a class.
+#pragma warning restore NS1000 // Non-virtual setup specification.
+                .Returns(Task.FromResult(okMessage));
+
+            using var httpClient = new HttpClient(messageHandler);
+            httpClient.BaseAddress = new Uri("http://test/v1/api/agent/service/");
+
+            var resource = CreateResource(httpClient);
+
+            // Act
+            Func<Task> deregister =
+                async () =>
+                    await resource
+                        .DeregisterAsync("service-id")
+                        .ConfigureAwait(false);
+
+            // Assert
+            await deregister.Should().NotThrowAsync().ConfigureAwait(false);
+        }
+
+        [DataTestMethod]
+        [DataRow(BadRequest)]
+        [DataRow(NotFound)]
+        [DataRow(InternalServerError)]
+        [DataRow(Forbidden)]
+        public async Task DeregisteringGivenUnsuccefullHttpResponseCodeShouldThrowHttpRequestExceptionWithExpectedErrorStatusCode(
+            HttpStatusCode expectedErrorStatusCode)
+        {
+            // Arrange
+            var agentServiceRegister = new AgentServiceRegister();
+
+            using var emptyContent = new StringContent(string.Empty);
+            using var messageHandler = Substitute.For<HttpMessageHandler>();
+            using var errorMessage = new HttpResponseMessage
+            {
+                Content = emptyContent,
+                StatusCode = expectedErrorStatusCode,
+            };
+
+            using var httpClient = new HttpClient(messageHandler);
+            httpClient.BaseAddress = new Uri("http://test/v1/api/agent/service/");
+
+#pragma warning disable NS1000 // Non-virtual setup specification.
+            _ = messageHandler
+                .Protected("SendAsync", default!, default!)
+#pragma warning restore NS1000 // Non-virtual setup specification.
+                .ReturnsForAnyArgs(Task.FromResult(errorMessage));
+
+            var resource = CreateResource(
+                httpClient);
+
+            // Act
+            Func<Task> deregisteringGivenUnsuccefullHttpResponseCode =
+                async () => await resource.RegisterAsync(agentServiceRegister).ConfigureAwait(false);
+
+            // Assert
+            _ = (await deregisteringGivenUnsuccefullHttpResponseCode
+                    .Should()
+                        .ThrowExactlyAsync<HttpRequestException>().ConfigureAwait(false))
+                    .Which
+                        .StatusCode.Should().Be(expectedErrorStatusCode);
+        }
 
         private static IAgentServiceResources CreateResource(
             HttpClient? httpClient = default,
